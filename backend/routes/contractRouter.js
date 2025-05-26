e6268bdcebdcf6667308ac6cfb5f75c199dd537b
Алина Дмитриевна <alinauglova66@gmail.com>
2025-05-27 00:16:09 +0300

    Update contractRouter.js

--- backend/routes/contractRouter.js
+++ backend/routes/contractRouter.js
@@ -3,7 +3,7 @@ import * as contractController from "../controllers/contractController.js";
 
 const router = express.Router();
 
-router.post("/contract", contactController.getContacts);
+// router.post("/contract", contactController.getContacts);
 // router.get("/patients", patientsController.getPatients);
 // router.post("/register", contactController.registerPatient);
 
